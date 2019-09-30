f5d785e2f5c0671f3f1258336b8f9969d72fae29
POFK <maotianxiang@bao.ac.cn>
2019-09-30 23:46:14 +0800

    feat(*): test 9

--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -28,9 +28,9 @@ if [[ "$DISTRIB" == "conda" ]]; then
         wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
             -O miniconda.sh
         chmod +x miniconda.sh && ./miniconda.sh -b -p $HOME/miniconda
-        conda init
     fi
     export PATH=$HOME/miniconda/bin:$PATH
+    conda init
     # Make sure to use the most updated version
     conda update --yes conda
 
@@ -39,7 +39,6 @@ if [[ "$DISTRIB" == "conda" ]]; then
     # (prefer local venv, since the miniconda folder is cached)
     conda create -n test --yes python=${PYTHON_VERSION} pip
     conda activate test
-    echo "4!"
 fi
 
 if [[ "$COVERAGE" == "true" ]]; then
